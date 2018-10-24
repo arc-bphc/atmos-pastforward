d39ccbe625541b1f47835b967c7ac9d413509850
Ujjwal Raizada <ujjwalraizada@gmail.com>
2018-10-24 13:31:48 +0530

    django-server: basic model of rounds

---
+++ django-server/pastforward/portal/arduino_functions.py
@@ -0,0 +1,12 @@
+import time
+
+def phase1():
+    # phase 1 code
+    # time.sleep(5)
+    return 10.00
+
+
+def phase2():
+    # phase2 round1 code
+    # time.sleep(5)
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
--- django-server/pastforward/portal/urls.py
+++ django-server/pastforward/portal/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('teams/', views.teams, name='teams'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
     path('registration/', views.registration, name='registration'),
+    path('gameplay/<slug:team_name>/', views.gameplay, name="gameplay"),
+    path('gameplay/<slug:team_name>/<int:phase>/', views.gameplay, name="gameplay"),
 ]
